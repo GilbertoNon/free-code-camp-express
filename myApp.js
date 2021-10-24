0fec23baea9ec60c6f9f1acb856f144f9e0519c2
gilberto <vazqueznoriegagilberto@gmail.com>
2021-10-24 17:24:31 -0500

    hello world.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 console.log('Hello World');
 app.use('/public', express.static(`${__dirname}/public`))
@@ -8,7 +9,12 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`)
 })
 
-app.get('/json', (req, res) => res.json({"message": "Hello json"}))
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message": "HELLO JSON"})
+    }    
+    res.json({"message": "Hello json"})
+})
 
 
 
